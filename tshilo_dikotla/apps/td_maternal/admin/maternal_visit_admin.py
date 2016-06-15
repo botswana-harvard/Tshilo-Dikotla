01f5bf3d8e2ac96bae696536574ff0b91c62d963
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2016-06-15 21:35:26 +0200

    fixed bug on redirecting to maternal dashboard from saving maternal visit

--- tshilo_dikotla/apps/td_maternal/admin/maternal_visit_admin.py
+++ tshilo_dikotla/apps/td_maternal/admin/maternal_visit_admin.py
@@ -3,14 +3,14 @@ from copy import copy
 
 from edc_visit_tracking.admin import VisitAdminMixin
 
-from tshilo_dikotla.apps.td.base_model_admin import BaseModelAdmin
+from tshilo_dikotla.apps.td.base_model_admin import MembershipBaseModelAdmin
 from tshilo_dikotla.apps.td_lab.models import MaternalRequisition
 
 from ..forms import MaternalVisitForm
 from ..models import MaternalVisit
 
 
-class MaternalVisitAdmin(VisitAdminMixin, BaseModelAdmin):
+class MaternalVisitAdmin(VisitAdminMixin, MembershipBaseModelAdmin):
 
     form = MaternalVisitForm
     visit_attr = 'maternal_visit'
