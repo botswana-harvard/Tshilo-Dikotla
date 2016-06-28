1877af4ecabaeab1e501359b4ccd46a6b61f376b
Pearl Malapeng <pearlmalapeng@gmail.com>
2016-06-28 16:05:27 +0200

    added 5 more models to the admin

--- tshilo_dikotla/apps/td_maternal/admin/maternal_medical_history_admin.py
+++ tshilo_dikotla/apps/td_maternal/admin/maternal_medical_history_admin.py
@@ -15,13 +15,16 @@ class MaternalMedicalHistoryAdmin(BaseMaternalModelAdmin):
 #               'who_diagnosis',
 # #               'who',
 #     )
-    list_display = ('maternal_visit', 'chronic_since', 'sero_posetive', 'date_hiv_diagnosis', 'perinataly_infected')
+    list_display = ('maternal_visit', 'chronic_since', 'sero_posetive', 'date_hiv_diagnosis', 'perinataly_infected',
+                    'lowest_cd4_known', 'cd4_count', 'cd4_date')
     list_filter = ('chronic_since', 'sero_posetive', 'date_hiv_diagnosis', 'perinataly_infected')
     radio_fields = {'chronic_since': admin.VERTICAL,
                     'who_diagnosis': admin.VERTICAL,
                     'sero_posetive': admin.VERTICAL,
                     'perinataly_infected': admin.VERTICAL,
-                    'know_hiv_status': admin.VERTICAL}
+                    'know_hiv_status': admin.VERTICAL,
+                    'lowest_cd4_known': admin.VERTICAL,
+                    'is_date_estimated': admin.VERTICAL}
     filter_horizontal = ('who', 'mother_chronic', 'father_chronic', 'mother_medications')
 
 admin.site.register(MaternalMedicalHistory, MaternalMedicalHistoryAdmin)
