2f692fb34d6952f404a6a822c48b76ad8c9dbcb8
Pearlidixie <pearlmalapeng@gmail.com>
2016-06-29 16:26:35 +0200

    fixed validation issues

--- tshilo_dikotla/apps/td_maternal/forms/maternal_substance_use_form.py
+++ tshilo_dikotla/apps/td_maternal/forms/maternal_substance_use_form.py
@@ -20,19 +20,23 @@ class MaternalSubstanceUseForm(BaseMaternalModelForm):
         cleaned_data = self.cleaned_data
         if cleaned_data.get('smoked_prior_to_preg') in [YES]:
             if not cleaned_data.get('smoking_prior_preg_freq'):
-                raise forms.ValidationError('Participant has smoked tobacco prior to this pregnancy, please give a frequency.')
+                raise forms.ValidationError(
+                    'Participant has smoked tobacco prior to this pregnancy, please give a frequency.')
         else:
             if cleaned_data.get('smoking_prior_preg_freq'):
-                raise forms.ValidationError('Participant has never smoked tobacco prior to this pregnancy, please do not give a frequency.')
+                raise forms.ValidationError(
+                    'Participant has never smoked tobacco prior to this pregnancy, please do not give a frequency.')
 
     def validate_smoked_during_pregnancy(self):
         cleaned_data = self.cleaned_data
-        if cleaned_data.get('smoked_during_to_preg') == YES:
+        if cleaned_data.get('smoked_during_pregnancy') == YES:
             if not cleaned_data.get('smoking_during_preg_freq'):
-                raise forms.ValidationError('Participant has smoked tobacco during to this pregnancy, please give a frequency.')
+                raise forms.ValidationError(
+                    'Participant has smoked tobacco during this pregnancy, please give a frequency.')
         else:
             if cleaned_data.get('smoking_during_preg_freq'):
-                raise forms.ValidationError('Participant has never smoked tobacco during to this pregnancy, please do not give a frequency.')
+                raise forms.ValidationError(
+                    'Participant has never smoked tobacco during this pregnancy, please do not give a frequency.')
 
     def validate_alcohol_during_pregnancy(self):
         cleaned_data = self.cleaned_data
