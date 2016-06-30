48b47fd33ed0ef5cd27c181b5e7c15c436ebe572
Leslie <leslie.kep@gmail.com>
2016-06-30 02:33:00 +0200

    added maternal contraception tests

--- tshilo_dikotla/apps/td_maternal/forms/maternal_contraception_form.py
+++ tshilo_dikotla/apps/td_maternal/forms/maternal_contraception_form.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from edc_constants.constants import YES
+from edc_constants.constants import YES, NO
 
 from ..models import MaternalContraception
 from .base_maternal_model_form import BaseMaternalModelForm
@@ -10,49 +10,67 @@ class MaternalContraceptionForm(BaseMaternalModelForm):
 
     def clean(self):
         cleaned_data = super(MaternalContraceptionForm, self).clean()
-#         self.validate_more_children()
-#         self.validate_next_child()
-#         self.validate_uses_contraceptive()
-#         self.validate_pap_smear()
-#         self.validate_pap_smear_result()
+        self.validate_more_children()
+        self.validate_next_child()
+        self.validate_uses_contraceptive()
+        self.validate_another_pregnancy()
+        self.validate_pap_smear()
+        self.validate_pap_smear_result()
         return cleaned_data
 
     def validate_more_children(self):
         cleaned_data = self.cleaned_data
         if not cleaned_data.get('more_children') == YES:
-            if (cleaned_data.get('next_child') or cleaned_data.get('contraceptive_measure') or
-               cleaned_data.get('contraceptive_partner') or cleaned_data.get('contraceptive_relative')):
+            if cleaned_data.get('next_child'):
                 raise forms.ValidationError(
-                    'Participant did not answer YES to wanting more children skip to Q7.')
+                    'You said the client does not desire more children please do not answer '
+                    'When would you like to have your next child?')
 
     def validate_next_child(self):
         cleaned_data = self.cleaned_data
-        if cleaned_data.get('next_child') not in ['within 2years', 'between 2-5years from now',
-                                                  'More than 5years from now', 'More than 5 years from now']:
-            if (cleaned_data.get('contraceptive_measure') or cleaned_data.get('contraceptive_partner') or
-               cleaned_data.get('contraceptive_relative') or cleaned_data.get('influential_decision_making')):
-                raise forms.ValidationError(
-                    'Participant answered next child with d or e, skip to question on contraceptive method.')
+        if cleaned_data.get('more_children') == YES:
+            if not cleaned_data.get('next_child'):
+                raise forms.ValidationError('Participant desires more children, question on next child cannot be None.')
 
     def validate_uses_contraceptive(self):
         cleaned_data = self.cleaned_data
         if cleaned_data.get('uses_contraceptive') == YES:
             if not cleaned_data.get('contr'):
                 raise forms.ValidationError('Participant uses a contraceptive method, please select a valid method')
+            if not cleaned_data.get('contraceptive_startdate'):
+                raise forms.ValidationError('Participant uses a contraceptive method, please give a contraceptive startdate.')
         else:
             if cleaned_data.get('contr'):
                 raise forms.ValidationError(
                     'Participant does not use a contraceptive method, no need to give a contraceptive method')
+            if cleaned_data.get('contraceptive_startdate'):
+                raise forms.ValidationError('Participant does not use a contraceptive method, no need to give a contraceptive startdate.')
+
+    def validate_another_pregnancy(self):
+        cleaned_data = self.cleaned_data
+        if cleaned_data.get('another_pregnancy') == YES:
+            if not cleaned_data.get('pregnancy_date'):
+                raise forms.ValidationError('Participant is pregnant, please give date participant found out.')
+        else:
+            if cleaned_data.get('pregnancy_date'):
+                raise forms.ValidationError('Participant is not pregnant, do not give a date.')
 
     def validate_pap_smear(self):
         cleaned_data = self.cleaned_data
         if cleaned_data.get('pap_smear') == YES:
             if not cleaned_data.get('pap_smear_date'):
                 raise forms.ValidationError('Please give the date the pap smear was done.')
+            if cleaned_data.get('pap_smear_date'):
+                if not cleaned_data.get('pap_smear_estimate'):
+                    raise forms.ValidationError(
+                        'Pap smear date has been provided, please let us know if this date has been estimated.')
+        elif cleaned_data.get('pap_smear') == NO:
+            if cleaned_data.get('pap_smear_date'):
+                raise forms.ValidationError('Pap smear date not known, please do not add it.')
         else:
-            if (cleaned_data.get('pap_smear_date'), cleaned_data.get('pap_smear_estimate') or
+            if (cleaned_data.get('pap_smear_date') or cleaned_data.get('pap_smear_estimate') or
                cleaned_data.get('pap_smear_result') or cleaned_data.get('pap_smear_result_status') or
-               cleaned_data.get('pap_smear_result_abnormal') or cleaned_data.get('date_notified')):
+               cleaned_data.get('pap_smear_result_abnormal') or cleaned_data.get('date_notified')) is not None:
                 raise forms.ValidationError('Pap smear not done please do not answer questions regarding pap smear.')
 
     def validate_pap_smear_result(self):
@@ -66,7 +84,7 @@ class MaternalContraceptionForm(BaseMaternalModelForm):
                cleaned_data.get('date_notified')):
                 raise forms.ValidationError(
                     'Participant pap smear result not known, no need to give pap smear status or notification date.')
-
+    
     class Meta:
         model = MaternalContraception
-        fields = '__all__'
+        fields = '__all__'     
