efa72399012ffd3ffd36437484dfe2eaac8a3fdf
Leslie <leslie.kep@gmail.com>
2016-07-13 12:03:36 +0200

    added manager methods

---
+++ td_maternal/managers/maternal_clinical_measurements_two_manager.py
@@ -0,0 +1,9 @@
+from django.db import models
+from edc_registration.models import RegisteredSubject
+
+
+class MaternalClinicalMeasurementsTwoManager(models.Manager):
+    
+    def get_by_natural_key(self, subject_identifier_as_pk):
+        registered_subject = RegisteredSubject.objects.get_by_natural_key(subject_identifier_as_pk)
+        return self.get(registered_subject=registered_subject)
--- td_maternal/models/maternal_clinical_measurements_one.py
+++ td_maternal/models/maternal_clinical_measurements_one.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+from edc_registration.models import RegisteredSubject
 
+from ..managers import MaternalClinicalMeasurementsOneManager
 from .base_maternal_clinical_measurements import BaseMaternalClinicalMeasurements
 
 
@@ -14,7 +16,14 @@ class MaternalClinicalMeasurementsOne(BaseMaternalClinicalMeasurements):
         validators=[MinValueValidator(134), MaxValueValidator(195), ],
         help_text="Measured in Centimeters (cm)")
 
-    objects = models.Manager()
+    objects = MaternalClinicalMeasurementsOneManager()
+    
+    def __str__(self):
+        return "{0}".format(self.registered_subject.subject_identifier)
+
+    def natural_key(self):
+        return self.registered_subject.natural_key()
+    natural_key.dependencies = ['edc_registration.registeredsubject']
 
     class Meta:
         app_label = 'td_maternal'
