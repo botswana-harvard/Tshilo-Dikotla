ef847f5564845d044e2843bbae9cf5e06e31cb66
TshepangRas <tshras123@gmail.com>
2016-07-13 15:33:29 +0200

    run flake8 in tests files

--- tshilo_dikotla/apps/td_maternal/tests/test_maternal_clinical_measurement_one.py
+++ tshilo_dikotla/apps/td_maternal/tests/test_maternal_clinical_measurement_one.py
@@ -10,17 +10,16 @@ class TestMaternalClinicalMeasurementOne(BaseTestCase):
         self.maternal_eligibility = MaternalEligibilityFactory()
         self.maternal_consent = MaternalConsentFactory(registered_subject=self.maternal_eligibility.registered_subject)
         self.registered_subject = self.maternal_consent.registered_subject
-        self.data ={
-        'systolic_bp' : 75,          
-        'diastolic_bp': 120           
-                    }
+        self.data = {'systolic_bp': 75,
+                     'diastolic_bp': 120, }
+
     def test_systolic_bp_lower_than_diastolic_bp(self):
         self.data['systolic_bp'] = 40
         self.data['diastolic_bp'] = 80
         form = MaternalClinicalMeasurementsOneForm(data=self.data)
         self.assertIn('Systolic blood pressure cannot be lower than the diastolic blood pressure.'
-                ' Please correct.',form.errors.get('__all__') )
-    
+                      ' Please correct.', form.errors.get('__all__'))
+
     def test_systolic_not_blank(self):
         """Test if the Systolic blood pressure field is not empty"""
         self.data['systolic_bp'] = None
--- tshilo_dikotla/apps/td_maternal/tests/test_rapid_test_form.py
+++ tshilo_dikotla/apps/td_maternal/tests/test_rapid_test_form.py
@@ -1,7 +1,7 @@
 from django.utils import timezone
 
 from edc_constants.constants import YES, NO, POS, NEG, IND
- 
+
 from .factories import MaternalEligibilityFactory
 from .factories import MaternalConsentFactory
 from td_maternal.forms import RapidTestResultForm, BaseMaternalModelForm
@@ -19,18 +19,17 @@ class TestRapidTestForm(BaseTestCase):
         self.maternal_consent = MaternalConsentFactory(
             registered_subject=self.maternal_eligibility.registered_subject)
         self.registered_subject = self.maternal_consent.registered_subject
-        self.data = {
-            'rapid_test_done': YES,
-            'result_date': timezone.now(),
-            'result': NEG,
-            }
+        self.data = {'rapid_test_done': YES,
+                     'result_date': timezone.now(),
+                     'result': NEG, }
+
     def test_result_date_provided(self):
         """Test if result date of the rapid test is provided"""
         self.data['rapid_test_done'] = YES
         self.data['result_date'] = None
         rapid_form = RapidTestResultForm(data=self.data)
         self.assertIn('If a rapid test was processed, what is the date'
-        ' of the rapid test?', rapid_form.errors.get('__all__'))
+                      ' of the rapid test?', rapid_form.errors.get('__all__'))
 
     def test_rapid_test_results(self):
         """Test if the result of rapid test is provided"""
@@ -39,7 +38,7 @@ class TestRapidTestForm(BaseTestCase):
         self.data['result'] = None
         rapid_form = RapidTestResultForm(data=self.data)
         self.assertIn('If a rapid test was processed, what is the test result?',
-        rapid_form.errors.get('__all__'))
+                      rapid_form.errors.get('__all__'))
 
     def test_result_date_present_no_rapid_test_result(self):
         """Test if there is a date for test and there is no test"""
@@ -48,9 +47,8 @@ class TestRapidTestForm(BaseTestCase):
         self.data['result_date'] = timezone.now()
         rapid_form = RapidTestResultForm(data=self.data)
         self.assertIn('If a rapid test was not processed, please do not provide the result date. '
-        'Got {}.'.format(result_date.strftime('%Y-%m-%d')),
-        rapid_form.errors.get('__all__'))
-    
+                      'Got {}.'.format(result_date.strftime('%Y-%m-%d')), rapid_form.errors.get('__all__'))
+
     def test_validate_rapid_test_not_done(self):
         """test if the rapid test is not done"""
         self.data['rapid_test_done'] = NO
@@ -58,19 +56,14 @@ class TestRapidTestForm(BaseTestCase):
         self.data['result'] = None
         rapid_form = RapidTestResultForm(data=self.data)
         rapid_form.is_valid()
-        self.assertFalse(rapid_form.is_valid()) 
-    
+        self.assertFalse(rapid_form.is_valid())
+
     def test_rapid_test_result_present_no_rapid_test_done(self):
         """Test if the results are present and there is no rapid test performed"""
-        
         self.data['rapid_test_done'] = NO
         self.data['result_date'] = None
         self.data['result'] = NEG
         rapid_form = RapidTestResultForm(data=self.data)
         errors = ''.join(rapid_form.errors.get('__all__'))
         self.assertIn('If a rapid test was not processed, please do not provide the result. '
-        'Got {}.'.format(self.data['result']), errors)
-     
-    
-        
-        
+                      'Got {}.'.format(self.data['result']), errors)
