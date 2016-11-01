de49947bcaed7aa7c57efce584790b31765f3043
KeletsoBotsalano <kbotsalano@bhp.org.bw>
2016-11-01 16:12:22 +0200

    Searching maternal eligibility by subject identifier

--- td_dashboard/views/search_dashboard_view.py
+++ td_dashboard/views/search_dashboard_view.py
@@ -1,25 +1,42 @@
 from django.contrib import admin
 from django.conf import settings
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
 from ..forms import MaternalEligibilityCrispyForm
 from td_maternal.models.maternal_eligibility import MaternalEligibility
 
 
-class SearchDasboardView(TemplateView):
-
+class SearchDasboardView(TemplateView, FormView):
+    form_class = MaternalEligibilityCrispyForm
     template_name = 'td_dashboard/search_dashboard.html'
 
+    def __init__(self, **kwargs):
+        self.maternal_eligibility = None
+        super(SearchDasboardView, self).__init__(**kwargs)
+
+    def form_valid(self, form):
+        if form.is_valid():
+            subject_identifier = form.cleaned_data['subject_identifier']
+            print(form.cleaned_data)
+            try:
+                self.maternal_eligibility = MaternalEligibility.objects.get(registered_subject__subject_identifier=subject_identifier)
+            except MaternalEligibility.DoesNotExist:
+                form.add_error('subject_identifier', 'Maternal eligibility not found. Please search again or add a new maternal eligibility.')
+            context = self.get_context_data(form=form)
+        return self.render_to_response(context)
+
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+        context = super(SearchDasboardView, self).get_context_data(**kwargs)
         context.update(
             title=settings.PROJECT_TITLE,
             project_name=settings.PROJECT_TITLE,
             site_header=admin.site.site_header,
-            form=MaternalEligibilityCrispyForm(),
             maternal_eligibilities=self.maternal_eligibilities
         )
         return context
 
     @property
     def maternal_eligibilities(self):
-        return MaternalEligibility.objects.all().order_by('-created')[:20]
+        if self.maternal_eligibility is None:
+            return MaternalEligibility.objects.all().order_by('-created')[:20]
+        else:
+            return self.maternal_eligibility
