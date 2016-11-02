b4865cc02816881f007cedd7ba2f9e4d24e7f2c6
KeletsoBotsalano <kbotsalano@bhp.org.bw>
2016-11-02 10:58:02 +0200

    infant search + maternal eligibility

--- td_dashboard/views/search_dashboard_view.py
+++ td_dashboard/views/search_dashboard_view.py
@@ -3,24 +3,35 @@ from django.conf import settings
 from django.views.generic import TemplateView, FormView
 from ..forms import MaternalEligibilityCrispyForm
 from td_maternal.models.maternal_eligibility import MaternalEligibility
+from td_infant.models.infant_birth import InfantBirth
+
+from edc_base.view_mixins import EdcBaseViewMixin
 
 
-class SearchDasboardView(TemplateView, FormView):
+class SearchDasboardView(EdcBaseViewMixin, TemplateView, FormView):
     form_class = MaternalEligibilityCrispyForm
     template_name = 'td_dashboard/search_dashboard.html'
 
     def __init__(self, **kwargs):
         self.maternal_eligibility = None
+        self.infant = None
         super(SearchDasboardView, self).__init__(**kwargs)
 
     def form_valid(self, form):
         if form.is_valid():
             subject_identifier = form.cleaned_data['subject_identifier']
-            print(form.cleaned_data)
             try:
-                self.maternal_eligibility = MaternalEligibility.objects.get(registered_subject__subject_identifier=subject_identifier)
+                self.maternal_eligibility = MaternalEligibility.objects.get(
+                    registered_subject__subject_identifier=subject_identifier)
+                subject_identifier_infant = subject_identifier + '-10'
+                try:
+                    self.infant = InfantBirth.objects.get(
+                        registered_subject__subject_identifier=subject_identifier_infant)
+                except InfantBirth.DoesNotExist:
+                    pass
             except MaternalEligibility.DoesNotExist:
-                form.add_error('subject_identifier', 'Maternal eligibility not found. Please search again or add a new maternal eligibility.')
+                form.add_error('subject_identifier',
+                               'Maternal eligibility not found. Please search again or add a new maternal eligibility.')
             context = self.get_context_data(form=form)
         return self.render_to_response(context)
 
@@ -32,6 +43,8 @@ class SearchDasboardView(TemplateView, FormView):
             site_header=admin.site.site_header,
             maternal_eligibilities=self.maternal_eligibilities
         )
+        context.update({
+            'infant': self.infant})
         return context
 
     @property
@@ -39,4 +52,4 @@ class SearchDasboardView(TemplateView, FormView):
         if self.maternal_eligibility is None:
             return MaternalEligibility.objects.all().order_by('-created')[:20]
         else:
-            return self.maternal_eligibility
+            return [self.maternal_eligibility]
