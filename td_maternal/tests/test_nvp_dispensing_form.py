328d60da8cd1f99605e8da4afc7ef9b0102a0a26
erikvw <ew2789@gmail.com>
2016-12-11 22:40:33 +0200

    nvp_dispensing passing

--- td_maternal/tests/test_nvp_dispensing_form.py
+++ td_maternal/tests/test_nvp_dispensing_form.py
@@ -1,5 +1,5 @@
 from datetime import date
-from django.test import TestCase
+from django.test import TestCase, tag
 
 from edc_constants.constants import YES, NO
 
@@ -8,6 +8,7 @@ from ..forms import NvpDispensingForm
 from .test_mixins import PosMotherMixin
 
 
+@tag('forms')
 class TestNvpDispensingForm(PosMotherMixin, TestCase):
 
     def setUp(self):
