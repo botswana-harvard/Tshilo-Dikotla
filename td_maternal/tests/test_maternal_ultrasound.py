e0b0700217cd8247edb29484c94a7d85656bd777
erikvw <ew2789@gmail.com>
2016-12-11 13:15:38 +0200

    update ultrasound tests

--- td_maternal/tests/test_maternal_ultrasound.py
+++ td_maternal/tests/test_maternal_ultrasound.py
@@ -15,25 +15,18 @@ from .test_mixins import PosMotherMixin
 
 class TestMaternalUltrasound(PosMotherMixin, TestCase):
 
-    def setUp(self):
-        super(TestMaternalUltrasound, self).setUp()
-        self.add_maternal_visit('1000M')
-
     def test_pass_eligibility_on_singleton_pregnancy(self):
         """Test antenatal Enrollment remains as eligible on singleton fetus ultrasound."""
-        maternal_visit = self.get_maternal_visit('1000M')
+        maternal_visit = self.add_maternal_visit('1000M')
         mommy.make_recipe(
             'td_maternal.maternalultrasoundinitial',
-            maternal_visit=maternal_visit,
-            est_edd_ultrasound=timezone.now().date() + relativedelta(weeks=20),
-            ga_by_ultrasound_wks=20,
-            ga_by_ultrasound_days=4)
+            maternal_visit=maternal_visit)
         antenatal_enrollment = AntenatalEnrollment.objects.get(subject_identifier=self.subject_identifier)
         self.assertTrue(antenatal_enrollment.is_eligible)
 
     def test_fail_eligibility_on_non_singleton_pregnancy(self):
         """Test antenatal Enrollment fails eligible on non-singleton fetus ultrasound."""
-        maternal_visit = self.get_maternal_visit('1000M')
+        maternal_visit = self.add_maternal_visit('1000M')
         mommy.make_recipe(
             'td_maternal.maternalultrasoundinitial',
             number_of_gestations=2,
@@ -46,7 +39,7 @@ class TestMaternalUltrasound(PosMotherMixin, TestCase):
 
     def test_ineligible_adds_offstudy(self):
         """Offstudy created on antenatal enrollment failure."""
-        maternal_visit = self.get_maternal_visit('1000M')
+        maternal_visit = self.add_maternal_visit('1000M')
         antenatal_enrollment = AntenatalEnrollment.objects.get(subject_identifier=self.subject_identifier)
         antenatal_enrollment.knows_lmp = NO,
         antenatal_enrollment.last_period_date = None
@@ -69,14 +62,8 @@ class TestMaternalUltrasound(PosMotherMixin, TestCase):
     def test_ga_by_lmp(self):
         """Test GA by LMP correctly calculated considering antenatal enrollment date at lmp and  ultrasound
         date."""
-        maternal_visit = self.get_maternal_visit('1000M')
-        antenatal_enrollment = AntenatalEnrollment.objects.get(subject_identifier=self.subject_identifier)
-        maternal_ultrasound = mommy.make_recipe(
-            'td_maternal.maternalultrasoundinitial',
-            maternal_visit=maternal_visit,
-            est_edd_ultrasound=antenatal_enrollment.edd_by_lmp + relativedelta(days=17),
-            ga_by_ultrasound_wks=22,
-            ga_by_ultrasound_days=3)
+        maternal_visit = self.add_maternal_visit('1000M')
+        maternal_ultrasound = self.make_ultrasound(maternal_visit)
         antenatal_enrollment = AntenatalEnrollment.objects.get(subject_identifier=self.subject_identifier)
         lmp = Lmp(
             lmp=antenatal_enrollment.last_period_date,
@@ -108,17 +95,13 @@ class TestMaternalUltrasound(PosMotherMixin, TestCase):
     def test_ga_edd_confirmed_with_no_antenatal_lmp(self):
         """Test GA and EDD confirmed are automatically chosen from ultrasound values
         if no LMP at antenatal enrollment"""
-        maternal_visit = self.get_maternal_visit('1000M')
+        maternal_visit = self.add_maternal_visit('1000M')
         antenatal_enrollment = AntenatalEnrollment.objects.get(subject_identifier=self.subject_identifier)
         antenatal_enrollment.knows_lmp = NO,
         antenatal_enrollment.last_period_date = None
         antenatal_enrollment.save()
-        maternal_ultrasound = mommy.make_recipe(
-            'td_maternal.maternalultrasoundinitial',
-            maternal_visit=maternal_visit,
-            report_datetime=maternal_visit.report_datetime,
-            est_edd_ultrasound=(maternal_visit.report_datetime + relativedelta(weeks=20)).date(),
-            ga_by_ultrasound_wks=20,
+        maternal_ultrasound = self.make_ultrasound(
+            maternal_visit,
             ga_by_ultrasound_days=4)
         self.assertEqual(maternal_ultrasound.edd_confirmed.date(), maternal_ultrasound.est_edd_ultrasound)
         antenatal_enrollment = AntenatalEnrollment.objects.get(subject_identifier=self.subject_identifier)
@@ -127,7 +110,7 @@ class TestMaternalUltrasound(PosMotherMixin, TestCase):
 
     def test_no_lmp_with_ultrasound_gaconfirmed_eligible(self):
         """Test if no LMP at antenatal enrollment, can still pass or fail eligibility based on ultrasound GA."""
-        maternal_visit = self.get_maternal_visit('1000M')
+        maternal_visit = self.add_maternal_visit('1000M')
         antenatal_enrollment = AntenatalEnrollment.objects.get(subject_identifier=self.subject_identifier)
         self.assertTrue(antenatal_enrollment.is_eligible)
         antenatal_enrollment.knows_lmp = NO,
@@ -135,20 +118,17 @@ class TestMaternalUltrasound(PosMotherMixin, TestCase):
         antenatal_enrollment.unenrolled = None
         antenatal_enrollment.save()
         antenatal_enrollment = AntenatalEnrollment.objects.get(subject_identifier=self.subject_identifier)
-        ultrasound = mommy.make_recipe(
-            'td_maternal.maternalultrasoundinitial',
-            maternal_visit=maternal_visit,
-            est_edd_ultrasound=(timezone.now() + relativedelta(weeks=20)).date(),
-            ga_by_ultrasound_wks=20,
+        maternal_ultrasound = self.make_ultrasound(
+            maternal_visit,
             ga_by_ultrasound_days=4)
-        self.assertIsNone(ultrasound.ga_by_lmp)
-        self.assertEqual(ultrasound.ga_by_ultrasound_wks, 20)
+        self.assertIsNone(maternal_ultrasound.ga_by_lmp)
+        self.assertEqual(maternal_ultrasound.ga_by_ultrasound_wks, 20)
         antenatal_enrollment = AntenatalEnrollment.objects.get(subject_identifier=self.subject_identifier)
         self.assertTrue(antenatal_enrollment.is_eligible)
 
     def test_no_lmp_with_ultrasound_gaconfirmed_ineligible16(self):
         """Test if no LMP at antenatal enrollment, can still pass or fail eligibility based on ultrasound GA."""
-        maternal_visit = self.get_maternal_visit('1000M')
+        maternal_visit = self.add_maternal_visit('1000M')
         antenatal_enrollment = AntenatalEnrollment.objects.get(subject_identifier=self.subject_identifier)
         self.assertTrue(antenatal_enrollment.is_eligible)
         antenatal_enrollment.knows_lmp = NO,
@@ -156,10 +136,8 @@ class TestMaternalUltrasound(PosMotherMixin, TestCase):
         antenatal_enrollment.unenrolled = None
         antenatal_enrollment.save()
         antenatal_enrollment = AntenatalEnrollment.objects.get(subject_identifier=self.subject_identifier)
-        mommy.make_recipe(
-            'td_maternal.maternalultrasoundinitial',
-            maternal_visit=maternal_visit,
-            est_edd_ultrasound=timezone.now().date() + relativedelta(weeks=24),
+        self.make_ultrasound(
+            maternal_visit,
             ga_by_ultrasound_wks=16,
             ga_by_ultrasound_days=4)
         antenatal_enrollment = AntenatalEnrollment.objects.get(subject_identifier=self.subject_identifier)
@@ -167,7 +145,7 @@ class TestMaternalUltrasound(PosMotherMixin, TestCase):
 
     def test_no_lmp_with_ultrasound_gaconfirmed_eligible36(self):
         """Test if no LMP at antenatal enrollment, can still pass or fail eligibility based on ultrasound GA."""
-        maternal_visit = self.get_maternal_visit('1000M')
+        maternal_visit = self.add_maternal_visit('1000M')
         antenatal_enrollment = AntenatalEnrollment.objects.get(subject_identifier=self.subject_identifier)
         self.assertTrue(antenatal_enrollment.is_eligible)
         antenatal_enrollment.knows_lmp = NO,
@@ -175,10 +153,8 @@ class TestMaternalUltrasound(PosMotherMixin, TestCase):
         antenatal_enrollment.unenrolled = None
         antenatal_enrollment.save()
         antenatal_enrollment = AntenatalEnrollment.objects.get(subject_identifier=self.subject_identifier)
-        mommy.make_recipe(
-            'td_maternal.maternalultrasoundinitial',
-            maternal_visit=maternal_visit,
-            est_edd_ultrasound=timezone.now().date() + relativedelta(weeks=4),
+        self.make_ultrasound(
+            maternal_visit,
             ga_by_ultrasound_wks=36,
             ga_by_ultrasound_days=4)
         antenatal_enrollment = AntenatalEnrollment.objects.get(subject_identifier=self.subject_identifier)
@@ -186,7 +162,7 @@ class TestMaternalUltrasound(PosMotherMixin, TestCase):
 
     def test_no_lmp_with_ultrasound_gaconfirmed_ineligible37(self):
         """Test if no LMP at antenatal enrollment, can still pass or fail eligibility based on ultrasound GA."""
-        maternal_visit = self.get_maternal_visit('1000M')
+        maternal_visit = self.add_maternal_visit('1000M')
         antenatal_enrollment = AntenatalEnrollment.objects.get(subject_identifier=self.subject_identifier)
         self.assertTrue(antenatal_enrollment.is_eligible)
         antenatal_enrollment.knows_lmp = NO,
@@ -194,10 +170,8 @@ class TestMaternalUltrasound(PosMotherMixin, TestCase):
         antenatal_enrollment.unenrolled = None
         antenatal_enrollment.save()
         antenatal_enrollment = AntenatalEnrollment.objects.get(subject_identifier=self.subject_identifier)
-        mommy.make_recipe(
-            'td_maternal.maternalultrasoundinitial',
-            maternal_visit=maternal_visit,
-            est_edd_ultrasound=timezone.now().date() + relativedelta(weeks=3),
+        self.make_ultrasound(
+            maternal_visit,
             ga_by_ultrasound_wks=37,
             ga_by_ultrasound_days=4)
         antenatal_enrollment = AntenatalEnrollment.objects.get(subject_identifier=self.subject_identifier)
