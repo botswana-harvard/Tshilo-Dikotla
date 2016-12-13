35728374bd129d676f55477d007ca3c3b5081fdd
Coulson <ckgathi@bhp.org.bw>
2016-12-13 21:29:34 +0200

    test_infant_feeding_form passing

--- td_infant/tests/test_infant_feeding_form.py
+++ td_infant/tests/test_infant_feeding_form.py
@@ -1,92 +1,40 @@
-from dateutil.relativedelta import relativedelta
 from datetime import date
-from django.utils import timezone
+from django.test import TestCase
+from model_mommy import mommy
 
-from edc_registration.models import RegisteredSubject
 from edc_constants.constants import POS, YES, NO, NOT_APPLICABLE
+from edc_visit_tracking.constants import SCHEDULED
 
 from td.models import Appointment
 
 from td_infant.forms import InfantFeedingForm
 
-from td_maternal.tests.factories import (MaternalUltraSoundIniFactory, MaternalEligibilityFactory,
-                                         MaternalConsentFactory, AntenatalEnrollmentFactory,
-                                         AntenatalVisitMembershipFactory, MaternalLabDelFactory,
-                                         MaternalVisitFactory)
-
-from .factories import (InfantBirthFactory, InfantVisitFactory, InfantBirthArvFactory)
-
-from td_maternal.tests import BaseTestCase
+from .test_mixins import InfantMixin
 
 
-class TestInfantFeedingForm(BaseTestCase):
+class TestInfantFeedingForm(InfantMixin, TestCase):
 
     def setUp(self):
         super(TestInfantFeedingForm, self).setUp()
-        self.maternal_eligibility = MaternalEligibilityFactory()
-        self.maternal_consent = MaternalConsentFactory(
-            maternal_eligibility=self.maternal_eligibility)
-        self.registered_subject = self.maternal_eligibility.registered_subject
 
-        self.assertEqual(RegisteredSubject.objects.all().count(), 1)
-        self.options = {
-            'registered_subject': self.registered_subject,
-            'current_hiv_status': POS,
-            'evidence_hiv_status': YES,
-            'will_get_arvs': YES,
-            'is_diabetic': NO,
-            'will_remain_onstudy': YES,
-            'rapid_test_done': NOT_APPLICABLE,
-            'last_period_date': (timezone.datetime.now() - relativedelta(weeks=25)).date()}
+        self.make_infant_birth(maternal_status=POS)
+        infant_appointment_2000 = Appointment.objects.get(subject_identifier=self.infant_identifier, visit_code='2000')
+        mommy.make_recipe(
+            'td_infant.infantvisit',
+            appointment=infant_appointment_2000,
+            report_datetime=infant_appointment_2000.appt_datetime,
+            reason=SCHEDULED)
 
-        self.antenatal_enrollment = AntenatalEnrollmentFactory(**self.options)
-        self.appointment = Appointment.objects.get(
-            subject_identifier=self.options.get('registered_subject'), visit_code='1000M')
-
-        self.maternal_visit_1000 = MaternalVisitFactory(appointment=self.appointment, reason='scheduled')
-
-        self.maternal_ultrasound = MaternalUltraSoundIniFactory(
-            maternal_visit=self.maternal_visit_1000,
-            number_of_gestations=1)
-
-        self.antenatal_visits_membership = AntenatalVisitMembershipFactory(
-            registered_subject=self.options.get('registered_subject'))
-        self.appointment = Appointment.objects.get(
-            subject_identifier=self.options.get('registered_subject'), visit_code='1010M')
-
-        MaternalVisitFactory(appointment=self.appointment, reason='scheduled')
-
-        self.appointment = Appointment.objects.get(
-            subject_identifier=self.registered_subject.subject_identifier, visit_code='1020M')
-        MaternalVisitFactory(appointment=self.appointment, reason='scheduled')
-
-        self.maternal_labour_del = MaternalLabDelFactory(registered_subject=self.registered_subject)
-
-        self.appointment = Appointment.objects.get(
-            subject_identifier=self.registered_subject.subject_identifier, visit_code='2000M')
-        MaternalVisitFactory(appointment=self.appointment, reason='scheduled')
-
-        infant_registered_subject = RegisteredSubject.objects.get(
-            relative_identifier=self.registered_subject.subject_identifier,
-            subject_type='infant')
-
-        self.assertTrue(RegisteredSubject.objects.all().count(), 2)
-
-        self.infant_birth = InfantBirthFactory(
-            registered_subject=infant_registered_subject,
-            maternal_labour_del=self.maternal_labour_del)
-
-        self.appointment = Appointment.objects.get(
-            subject_identifier=infant_registered_subject.subject_identifier, visit_code='2000')
-        self.infant_visit = InfantVisitFactory(appointment=self.appointment)
-        self.infant_birth_arv = InfantBirthArvFactory(infant_visit=self.infant_visit, azt_discharge_supply=YES)
-        self.appointment = Appointment.objects.get(
-            subject_identifier=infant_registered_subject.subject_identifier, visit_code='2010')
-
-        self.infant_visit = InfantVisitFactory(appointment=self.appointment, reason='scheduled')
+        infant_appointment_2010 = Appointment.objects.get(subject_identifier=self.infant_identifier, visit_code='2010')
+        mommy.make_recipe(
+            'td_infant.infantvisit',
+            appointment=infant_appointment_2010,
+            report_datetime=infant_appointment_2010.appt_datetime,
+            reason=SCHEDULED)
+        self.make_infant_birth_arv(infant_visit=self.get_infant_visit('2000'))
 
         self.options = {
-            'infant_visit': self.infant_visit.id,
+            'infant_visit': self.get_infant_visit('2010').id,
             'other_feeding': YES,
             'formula_intro_occur': YES,
             'formula_intro_date': date.today(),
