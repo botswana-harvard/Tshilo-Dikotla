6023ea72d56a30d3444c2ea1f5dbac385f73a8f7
Leslie <leslie.kep@gmail.com>
2018-02-27 19:51:26 +0200

    remove repeating code

--- tshilo_dikotla/td_previous_visit_mixin.py
+++ tshilo_dikotla/td_previous_visit_mixin.py
@@ -9,27 +9,37 @@ class TdPreviousVisitMixin(PreviousVisitMixin):
 
         Only selects visit definition instances for this visit model."""
         consent_obj = apps.get_model('td_maternal', 'MaternalConsent')
-        visit_def_obj = apps.get_model('edc_visit_schedule', 'VisitDefinition')
+
         maternal_consent = consent_obj.objects.filter(
             maternal_eligibility__registered_subject__subject_identifier=self.appointment.registered_subject.subject_identifier).last()
 
+        if not maternal_consent:
+            maternal_consent = consent_obj.objects.filter(
+                maternal_eligibility__registered_subject__subject_identifier=self.appointment.registered_subject.relative_identifier).last()
+
         if maternal_consent.version == '1':
-            previous_visit_definition = visit_def_obj.objects.filter(
-                time_point__lt=visit_definition.time_point,
-                visit_tracking_content_type_map__app_label=self._meta.app_label,
-                visit_tracking_content_type_map__module_name=self._meta.model_name,
-                instruction='V1').order_by(
-                    'time_point', 'base_interval').last()
+            previous_visit_definition = self.previous_visit_definition(
+                instruction='V1', visit_definition=visit_definition)
         elif maternal_consent.version == '3':
-            previous_visit_definition = visit_def_obj.objects.filter(
-                time_point__lt=visit_definition.time_point,
-                visit_tracking_content_type_map__app_label=self._meta.app_label,
-                visit_tracking_content_type_map__module_name=self._meta.model_name,
-                instruction='V3').order_by(
-                'time_point', 'base_interval').last()
+            previous_visit_definition = self.previous_visit_definition(
+                instruction='V1', visit_definition=visit_definition)
+            if not previous_visit_definition:
+                self.previous_visit_definition(
+                    instruction='V3', visit_definition=visit_definition)
         if previous_visit_definition:
             return previous_visit_definition
         return None
+
+    def get_visit_definition(self, instruction, visit_definition):
+        visit_def_obj = apps.get_model('edc_visit_schedule', 'VisitDefinition')
+
+        previous_visit_definition = visit_def_obj.objects.filter(
+            time_point__lt=visit_definition.time_point,
+            visit_tracking_content_type_map__app_label=self._meta.app_label,
+            visit_tracking_content_type_map__module_name=self._meta.model_name,
+            instruction=instruction).order_by(
+            'time_point', 'base_interval').last()
+        return previous_visit_definition
 
     class Meta:
         abstract = True
