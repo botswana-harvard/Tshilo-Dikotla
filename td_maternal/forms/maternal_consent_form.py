ff780285523d44eba4d14bf364bd1b8ab1ab77a0
Gofaone Magodi <magodign@bhp.org.bw>
2018-03-08 12:08:38 +0200

    resolve for redmine 4080

--- td_maternal/forms/maternal_consent_form.py
+++ td_maternal/forms/maternal_consent_form.py
@@ -30,6 +30,8 @@ class MaternalConsentForm(BaseConsentForm):
         if cleaned_data.get('identity_type') == OMANG and cleaned_data.get('identity')[4] != '2':
             raise forms.ValidationError('Identity provided indicates participant is Male. Please correct.')
         eligibility = cleaned_data.get('maternal_eligibility')
+        if eligibility is None:
+                raise forms.ValidationError("Select age in Maternal eligibility")
         if cleaned_data.get('citizen') != eligibility.has_omang:
             raise forms.ValidationError(
                 "In eligibility you said has_omang is {}. Yet you wrote citizen is {}. "
