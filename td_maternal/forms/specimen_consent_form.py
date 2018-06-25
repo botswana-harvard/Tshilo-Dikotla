9b0b6dcfdd20fa3c6c381159292507b763e5cca4
Leslie <leslie.kep@gmail.com>
2018-06-25 16:57:44 +0200

    override consent validation method

--- td_maternal/forms/specimen_consent_form.py
+++ td_maternal/forms/specimen_consent_form.py
@@ -1,3 +1,4 @@
+from django import forms
 from edc_consent.forms import BaseSpecimenConsentForm
 
 from ..models import SpecimenConsent, MaternalConsent
@@ -10,6 +11,19 @@ class SpecimenConsentForm(BaseSpecimenConsentForm):
     def clean(self):
         cleaned_data = super(SpecimenConsentForm, self).clean()
         return cleaned_data
+
+    def study_consent_or_raise(self):
+        """Returns an instance of the current maternal consent or
+        raises an exception if not found."""
+        cleaned_data = self.cleaned_data
+        subject_identifier = cleaned_data.get(
+            'registered_subject').subject_identifier
+        maternal_consent = self.STUDY_CONSENT.objects.filter(
+            maternal_eligibility__registered_subject__subject_identifier=subject_identifier).order_by('consent_datetime').last()
+        if not maternal_consent:
+            raise forms.ValidationError(
+                'Maternal consent must be completed before the specimen consent.')
+        return maternal_consent
 
     class Meta:
         model = SpecimenConsent
