6cbd34ecc3312a7c96b107a390807e39bc2a8cdf
Leslie <leslie.kep@gmail.com>
2018-09-18 22:02:19 +0200

    fix redirect after consent and reconsent

--- tshilo_dikotla/base_model_admin.py
+++ tshilo_dikotla/base_model_admin.py
@@ -14,8 +14,17 @@ class BaseModelAdmin(ModelAdminNextUrlRedirectMixin, ModelAdminFormInstructionsM
 
     def redirect_url(self, request, obj, post_url_continue=None):
         url_name = request.GET.get(self.querystring_name)
-        section_name = request.GET.get('section_name')
-        return reverse(url_name, kwargs={'section_name': section_name})
+        dashboard_type = request.GET.get('dashboard_type')
+        dashboard_model = request.GET.get('dashboard_model')
+        dashboard_id = request.GET.get('dashboard_id')
+        show = request.GET.get('show')
+        instruction = request.GET.get('instruction')
+        return reverse(url_name, kwargs={
+            'dashboard_type': dashboard_type,
+            'dashboard_model': dashboard_model,
+            'dashboard_id': dashboard_id,
+            'show': show,
+            'instruction': instruction})
 
 
 class MembershipBaseModelAdmin(ModelAdminNextUrlRedirectMixin, ModelAdminFormInstructionsMixin,
