e5de35b609d7bc93650ac3bd0030c49ffd433196
tumiemosweu <imosweuk@gmail.com>
2018-12-11 18:34:50 +0200

    bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='tshilo_dikotla',
-    version='2.0.9',
+    version='2.1.0',
     author='Coulson Thabo Kgathi',
     author_email='ckgathi@gmail.com',
     packages=find_packages(),
